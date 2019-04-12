b16e9f5f3f63ba8074723241743df9dd9a26253a
Alumno Facultad de Ingenieria L1 <alum.fial1@lim.upeu.edu.pe>
2019-04-11 20:52:14 -0500

    Version 1

---
+++ js/javascript.js
@@ -0,0 +1,51 @@
+document.getElementById("inicio").style.display = "block"
+document.getElementById("ventas").style.display="none"
+document.getElementById("reportes").style.display="none"
+document.getElementById("clientes").style.display="none"
+document.getElementById("productos").style.display="none"
+
+
+function inicio(){
+    document.getElementById("inicio").style.display = "block"
+    document.getElementById("ventas").style.display="none"
+    document.getElementById("reportes").style.display="none"
+    document.getElementById("clientes").style.display="none"
+    document.getElementById("productos").style.display="none"
+    
+    
+
+
+}
+function ventas(){
+    document.getElementById("inicio").style.display = "none"
+    document.getElementById("ventas").style.display="block"
+    document.getElementById("reportes").style.display="none"
+    document.getElementById("clientes").style.display="none"
+    document.getElementById("productos").style.display="none"
+
+
+}
+function reportes(){
+    document.getElementById("inicio").style.display = "none"
+    document.getElementById("ventas").style.display="none"
+    document.getElementById("reportes").style.display="block"
+    document.getElementById("clientes").style.display="none"
+    document.getElementById("productos").style.display="none"
+}    
+
+function clientes(){
+    document.getElementById("inicio").style.display = "none"
+    document.getElementById("ventas").style.display="none"
+    document.getElementById("reportes").style.display="none"
+    document.getElementById("clientes").style.display="block"
+    document.getElementById("productos").style.display="none"
+
+}
+function productos(){
+    document.getElementById("inicio").style.display = "none"
+    document.getElementById("ventas").style.display="none"
+    document.getElementById("reportes").style.display="none"
+    document.getElementById("clientes").style.display="none"
+    document.getElementById("productos").style.display="block"
+
+}
